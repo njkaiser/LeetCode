da9d6cadfbbcbdd9c55b845ebfbb852a9bf4783a
Nate Kaiser <natejkaiser@gmail.com>
2017-03-28 18:15:08 -0500

    optimized two-sum, now in top decile

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,76 +1,56 @@
-// Given nums = [2, 7, 11, 15], target = 9,
+// Given an array of integers, return indices of the two numbers such that
+// they add up to a specific target.
 //
+// You may assume that each input would have exactly one solution, and you
+// may not use the same element twice.
+//
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
 
 #include <vector>
+#include <unordered_map>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
 
+
 class Solution {
-
 public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        int i = 0;
-        int j = nums.size();
-        vector<int> comps(j);
-        vector<int> ret;
+  vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> hashmap;
+    unordered_map<int, int>::iterator loc;
+    vector<int> output(2);
+    int comp;
 
-        // while(i < j)
-        // {
-        //     int sum = nums[i] + nums[j];
-        //     if(sum == target)
-        //         return {i, j};
-        //     else if(sum > target)
-        //     {
-        //         // comps.push_back()
-        //         // --j;
-        //     }
-        //     else if(sum < target)
-        //     {
-        //         // ++i;
-        //     }
-        // }
-
-        // int m = 0;
-        comps[0] = nums[0];
-        for(int k = 1; k < nums.size(); ++k)
-        {
-            for(int m = 0; m < k; ++m)
-            {
-              cout << "m, k = " << m << ", " << k << endl;
-              if(nums[k] + comps[m] == target)
-                {
-                  cout << "made it in the loop: " << m << ", " << k << endl;
-                  ret.push_back(m);
-                  ret.push_back(k);
-                  return ret;
-                }
-              else
-                comps[k] = (nums[k]);
-                cout << "comps:" << endl;
-                for(vector<int>::iterator it = comps.begin(); it != comps.end(); ++it)
-                  cout << *it << endl;
-            }
-        }
+    for(int i = 0; i < nums.size(); ++i) {
+      comp = target - nums[i];
+      loc = hashmap.find(comp);
+      if(loc != hashmap.end()) {
+        output[0] = loc->second;
+        output[1] = i;
+        return output;
+      } else {
+        hashmap[nums[i]] = i;
+      }
     }
+  }
 };
 
 
 int main(int argc, char** argv)
 {
-    static const int arr[] = {2, 5, 5, 11};
-    vector<int> input(arr, arr + sizeof(arr) / sizeof(arr[0]));
+  static const int arr[] = {2, 5, 5, 11};
+  vector<int> input(arr, arr + sizeof(arr) / sizeof(arr[0]));
 
-    int target = 10;
+  int target = 10;
 
-    Solution sol;
-    vector<int> answer = sol.twoSum(input, target);
+  Solution sol;
+  vector<int> answer = sol.twoSum(input, target);
 
-    cout << "answer =" << endl;
-    for(vector<int>::iterator it = answer.begin(); it != answer.end(); ++it)
-      cout << *it << endl;
+  cout << "answer =" << endl;
+  for(vector<int>::iterator it = answer.begin(); it != answer.end(); ++it)
+  cout << *it << endl;
 }
