da60b7f0f06979848c9eb6259f92a1c191745e6e
Nate Kaiser <natejkaiser@gmail.com>
2017-09-14 00:17:02 -0500

    pushing back to work on laptop tomorrow

--- 054-spiral-matrix.cpp
+++ 054-spiral-matrix.cpp
@@ -22,69 +22,32 @@ public:
     int left = 0;
     int right = matrix[0].size()-1;
     // if(right == 0) { return matrix[0]; }
-    vector<int> limits {top, left, bot, right}
+    vector<int> limits {left, top, right, bot};
     vector< pair<int, int> > delta { pair<int, int>(1, 0), pair<int, int>(0, 1), pair<int, int>(-1, 0), pair<int, int>(0, -1) };
 
     vector<int> output((bot+1)*(right+1));
     // cout << "output.size(): " << output.size() << endl;
 
-    // cout << "[BEFORE] top, bot, left, right: " << top << bot << left << right << endl;
+    cout << "top, bot, left, right: " << top << ", " << bot << ", " << left << ", " << right << endl;
 
     int count = 0;
     int idx = 0;
     int x = 0, y = 0;
-    while(idx < output.size()) {
-      for(int i = limits[idx%2]; i < limits[idx%2+2]; i++) {
-        cout << "x, y: " << x << ", " << y << endl;
-        x += delta[idx%4].first;
-        y += delta[idx%4].second;
+    while(idx < output.size()-1) {
+      cout << "[" << idx << "] going from " << limits[count%4] << " to " << limits[(count+2)%4] << endl;
+      for(int i = limits[count%4]; i <= limits[(count+2)%4]; i++) {
+        cout << "i: " << i << endl;
+        cout << "[" << idx << "] \tx, y: " << x << ", " << y << endl;
+        x += delta[count%4].first;
+        y += delta[count%4].second;
         idx++;
       }
+      // cout << "[" << idx << "] \tx, y: " << x << ", " << y << endl;
       count++;
+      if(count%2) { limits[count%4] += delta[count%4].second; }
+      else        { limits[count%4] += delta[count%4].first; }
+      if(count > idx) { break; } // SAFETY
     }
-
-
-
-
-    // while(idx < output.size()) {
-    //   int old = idx;
-    //
-    //   for(int i = left; i <= right; i++) {
-    //     cout << "left to right" << endl;
-    //     output[idx++] = matrix[top][i];
-    //     // cout << output[idx-1] << endl;
-    //   }
-    //   top++;
-    //   cout << "top, bot, left, right: " << top << bot << left << right << endl;
-    //
-    //   for(int i = top; i <= bot; i++) {
-    //     cout << "top to bot" << endl;
-    //     output[idx++] = matrix[i][right];
-    //     // cout << output[idx-1] << endl;
-    //   }
-    //   right--;
-    //   cout << "top, bot, left, right: " << top << bot << left << right << endl;
-    //
-    //   for(int i = right; i >= left; i--) {
-    //     cout << "left to right" << endl;
-    //     output[idx++] = matrix[bot][i];
-    //     // cout << output[idx-1] << endl;
-    //   }
-    //   bot--;
-    //   cout << "top, bot, left, right: " << top << bot << left << right << endl;
-    //
-    //   for(int i = bot; i >= top; i--) {
-    //     cout << "bot to top" << endl;
-    //     output[idx++] = matrix[i][left];
-    //     // cout << output[idx-1] << endl;
-    //   }
-    //   left++;
-    //   cout << "top, bot, left, right: " << top << bot << left << right << endl;
-    //
-    //   if(idx == old) { break; } // done
-    //
-    // }
-
     return output;
   }
 };
@@ -92,12 +55,12 @@ public:
 
 int main(int argc, char** argv)
 {
-  // vector<vector<int> > input { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+  vector<vector<int> > input { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
   // vector<vector<int> > input;
   // vector<vector<int> > input { {6, 9, 7} };
   // vector<vector<int> > input { {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} };
   // vector<vector<int> > input { {2, 5, 8}, {4, 0, -1} };
-  vector<vector<int> > input { {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10} };
+  // vector<vector<int> > input { {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10} };
 
   Solution sol;
   vector<int> answer = sol.spiralOrder(input);
