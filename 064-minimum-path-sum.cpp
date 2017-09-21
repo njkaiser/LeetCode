1083e19e134bc8f6f6d7f2051b39fa01937f7746
Nate Kaiser <natejkaiser@gmail.com>
2017-09-20 23:55:38 -0500

    6 ms! technically uses more memory than my original, but doesn't require modifying input

--- 064-minimum-path-sum.cpp
+++ 064-minimum-path-sum.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <vector>
+#include <numeric> // accumulate()
 #include <limits.h>
 
 using namespace std;
@@ -15,22 +16,16 @@ public:
   int minPathSum(vector< vector<int> >& grid) {
     int ymax = grid.size();
     int xmax = grid[0].size();
-    if(xmax == 1 && ymax == 1) { return grid[0][0]; }
+    if(ymax == 1) { return accumulate(grid[0].begin(), grid[0].end(), 0); }
 
     vector<int> curr_row(xmax);
     vector<int> last_row(grid[0]);
-
-    // cout << "\nas we go:" << endl;
     for(int x = 1; x < xmax; ++x) { last_row[x] += last_row[x-1]; }
     for(int y = 1; y < ymax; y++) {
       curr_row[0] = grid[y][0] + last_row[0];
       for(int x = 1; x < xmax; x++) {
         curr_row[x] = grid[y][x] + min(curr_row[x-1], last_row[x]);
       }
-      // for(int i = 0; i < xmax; ++i) {
-      //   cout << curr_row[i] << ", ";
-      // }
-      // cout << endl;
       last_row = curr_row;
     }
 
@@ -41,9 +36,11 @@ public:
 
 int main(int argc, char** argv)
 {
-  vector<vector<int> > grid { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+  // vector<vector<int> > grid { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
   // vector<vector<int> > grid { {1} };
   // vector<vector<int> > grid { };
+  // vector<vector<int> > grid { {9, 1, 4, 8} };
+  vector<vector<int> > grid { {9}, {1}, {4}, {8} };
 
   cout << "input:" << endl;
   for(int y = 0; y < grid.size(); y++) {
@@ -55,14 +52,6 @@ int main(int argc, char** argv)
 
   Solution sol;
   int ans = sol.minPathSum(grid);
-
-  // cout << "output:" << endl;
-  // for(int y = 0; y < grid.size(); y++) {
-  //   for(int x = 0; x < grid[0].size(); x++) {
-  //     cout << grid[y][x] << ", ";
-  //   }
-  //   cout << endl;
-  // }
 
   cout << "min path sum = " << ans << endl;
 
